dabe2faa02d31a25d392ca75248b41ce69b3d29e
yunju_ustc <cyjlnk@foxmail.com>
2021-10-28 15:54:14 +0800

    [1维粒子模拟 version3.6]成功调试read input parameters

--- input_param.m
+++ input_param.m
@@ -13,5 +13,73 @@ function prm = input_param(input_filename)
     return
   end
   
+    
+  
+  for l = 1:length(str1)
+    value = eval(char(str2(l)));
+    prmname= char(strread(char(str1(l)),'%s'));
+    switch prmname
+    case 'dx'
+      prm.dx = value;
+    case 'dt'
+      prm.dt = value;
+    case 'nx'
+      prm.nx = value;
+    case 'ntime'
+      prm.ntime = value;
+    case 'nplot'
+      prm.nplot = value;
+    case 'cv'
+      prm.cv = value;
+    case 'wc'
+      prm.wc = value;
+    case 'ajamp'
+      prm.ajamp = value;
+    case 'eamp'
+      prm.eamp = value;
+    case 'emax'
+      prm.emax = value;
+    case 'bamp'
+      prm.bamp = value;
+    case 'bmax'
+      prm.bmax = value;
+    case 'iex'
+      prm.iex = value;
+    case 'vmax'
+      prm.vmax = value;
+    case 'nv'
+      prm.nv = value;
+    case 'wj'
+      prm.wj = value;
+    case 'ns'
+      prm.ns = value;
+    case 'np'
+     prm.np = value;
+    case 'wp'
+      prm.wp = value;
+    case 'qm'
+      prm.qm = value;
+    case 'vpa'
+      prm.vpa = value;
+    case 'vpe'
+      prm.vpe = value;
+    case 'vd'
+      prm.vd = value;
+    case 'pch'
+      prm.pch = value;
+    case 'icolor'
+      prm.icolor = value;
+    case 'iparam'
+      prm.iparam = value;
+    case 'diagtype'
+      prm.diagtype = value;
+    case 'angle'
+      prm.angle = value;
+    otherwise
+      %disp(sprintf('Plese check input parameter %s.',prmname))
+    end
+  end
+
+return;
   
 end
